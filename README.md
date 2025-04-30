fef2498b0a06a9b51adf9932ae71a88182a6b5de
vyskochka <zaluckaev.a@yandex.ru>
2025-04-30 17:53:12 +0300

    update getDiff.js and add new asciinema

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/abea2e585b868008bedc/maintainability)](https://codeclimate.com/github/vyskochka/frontend-project-46/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/abea2e585b868008bedc/test_coverage)](https://codeclimate.com/github/vyskochka/frontend-project-46/test_coverage)
 
-[![asciicast](https://asciinema.org/a/gHAuDd9nXPg6NzxEpQVteocqq.svg)](https://asciinema.org/a/gHAuDd9nXPg6NzxEpQVteocqq)
+[![asciicast](https://asciinema.org/a/qmorpQ9Si1HfmFdxtL5faxATa.svg)](https://asciinema.org/a/qmorpQ9Si1HfmFdxtL5faxATa)
--- src/getDiff.js
+++ src/getDiff.js
@@ -3,29 +3,30 @@ import path from 'path';
 import _ from 'lodash';
 
 const readFile = (fileName) => {
-  const pathFile = path.resolve(fileName);
-  return JSON.parse(fs.readFileSync(pathFile, 'UTF-8'));
+  const pathFile = path.resolve(fileName); // делаем абсолютный путь
+  return JSON.parse(fs.readFileSync(pathFile, 'UTF-8')); // хавает строку в формате json, возвращает obj
 };
 
 const getDiff = (fileName1, fileName2) => {
-  const dan1 = readFile(fileName1);
-  const dan2 = readFile(fileName2);
-  const unionKeys = _.union(Object.keys(dan1), Object.keys(dan2));
-  const str = unionKeys.map((key) => {
-    if (Object.hasOwn(dan1, key) && !Object.hasOwn(dan2, key)) {
-      return `  - ${key}: ${dan1[key]}`;
-    } if (!Object.hasOwn(dan1, key) && Object.hasOwn(dan2, key)) {
-      return `  + ${key}: ${dan2[key]}`;
-    // отсекли варианты что каких-то ключей нет, значит остальное все есть
-    } if (dan1[key] === dan2[key]) {
-      return `    ${key}: ${dan1[key]}`;
-    } // а тут ключи есть, а значения разные
-    return `  - ${key}: ${dan1[key]}\n  + ${key}: ${dan2[key]}`;
-  });
-  return `{\n${str.join('\n')}\n}`;
+  const data1 = readFile(fileName1);
+  const data2 = readFile(fileName2);
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.union(keys1, keys2).sort(); // объединение ключей
+  const result = keys.map((key) => {
+    if (!Object.hasOwn(data1, key)) {
+      return `  + ${key}: ${data1[key]}`; // added
+    } else if (!Object.hasOwn(data2, key)) {
+      return `  - ${key}: ${data2[key]}`; // deleted
+    } else if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`; // changed
+    } else {
+      return `    ${key}: ${data1[key]}`; // unchanged
+    }
+  })
+  return `{\n${result.join('\n')}\n}`; // склеили в "типо JSON"
 };
 
-console.log('');
-const unusedVar = 42; // Это должно вызвать предупреждение о неиспользуемой переменной
+getDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
 
 export { getDiff, readFile };
